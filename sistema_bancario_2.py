598edaebdc6d6437e3026b6a1911638c6731291c
Stéphanie Rachel <stephrachel12@gmail.com>
2024-05-28 16:03:11 -0300

    Update sistema_bancario_2.py
    Definição inicial das funções do programa.

---
+++ sistema_bancario_2.py
@@ -0,0 +1,15 @@
+def saque(*,saldo, valor, extrato, limite, numero_saques, limite_saques):
+    return saldo, extrato
+
+def deposito(saldo, valor, extrato,/):
+    return saldo, extrato
+
+def extrato(saldo, /, *, extrato):
+    return saldo, extrato
+
+def cadastrar_usuario():
+    return None
+
+def cadastrar_conta_corrente():
+    return None
+
