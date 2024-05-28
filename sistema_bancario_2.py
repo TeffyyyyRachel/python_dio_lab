966703769f3b3df778004d5821788d97668324f0
Stéphanie Rachel <stephrachel12@gmail.com>
2024-05-28 17:16:02 -0300

    Update sistema_bancario_2.py
    Reorganizações e criação de mais variáveis.

--- sistema_bancario_2.py
+++ sistema_bancario_2.py
@@ -13,7 +13,8 @@ def cadastrar_usuario(nome, data_nascimento, cpf, endereco, usuario):
 def cadastrar_conta_corrente(agencia, numero_conta, usuario):
     return None
 
-menu = """
+def menu():
+    menu = """
 
     ==== MENU ====
 
@@ -27,32 +28,46 @@ menu = """
     ==============
 
     Operação desejada: """
+    return input(menu)
 
-while True:
-    operacao_escolhida = input(menu)
+def main():
+    AGENCIA = "0002"
+    LIMITE_DIARIO = 3
 
     saldo = 0
+    valor_sacado_dia = 0
+    numero_saques = 0
+    limite_diario = 500
+    extrato_mensagens = ""
+    usuario = []
+    contas = []
 
-    if operacao_escolhida == "1": # depósito
-        deposito()
-    
-    elif operacao_escolhida == "2": # saque
-        if saldo == 0:
-            print("\n     == Conta zerada. Faça um depósito antes de sacar. ==")
+    while True:
+        operacao_escolhida = menu()
+        
+
+        if operacao_escolhida == "1": # depósito
+            deposito()
+        
+        elif operacao_escolhida == "2": # saque
+            if saldo == 0:
+                print("\n     == Conta zerada. Faça um depósito antes de sacar. ==")
+            else:
+                saque()
+        
+        elif operacao_escolhida == "3": # extrato
+            extrato()
+
+        elif operacao_escolhida == "4": # cadastro de usuário
+            cadastrar_usuario()
+
+        elif operacao_escolhida == "5": # criação de nova conta
+            cadastrar_conta_corrente()
+        
+        elif operacao_escolhida == "0":
+            break
+
         else:
-            saque()
-    
-    elif operacao_escolhida == "3": # extrato
-        extrato()
+            print("Escolha um número válido, entre 0 e 5.")
 
-    elif operacao_escolhida == "4": # cadastro de usuário
-        cadastrar_usuario()
-
-    elif operacao_escolhida == "5": # criação de nova conta
-        cadastrar_conta_corrente()
-    
-    elif operacao_escolhida == "0":
-        break
-
-    else:
-        print("Escolha um número válido, entre 0 e 5.")
+main()
