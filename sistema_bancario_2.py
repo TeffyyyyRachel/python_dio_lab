dbdb6c62a92be078072c3d5720aecd3867eb0a97
Stéphanie Rachel <stephrachel12@gmail.com>
2024-05-28 18:37:31 -0300

    Update sistema_bancario_2.py
    Finalização das funções de depósito, saque e exibição do extrato.

--- sistema_bancario_2.py
+++ sistema_bancario_2.py
@@ -1,10 +1,41 @@
 def saque(*,saldo, valor, extrato, limite, numero_saques, limite_saques):
-    return saldo, extrato
+    excedeu_limite = valor > limite
+    excedeu_saldo = valor > saldo
+    excedeu_saques = numero_saques >= limite_saques
+
+    if excedeu_limite:
+        print("\n\tOPERAÇÃO FALHOU!!! Saque excede o limite diário de R$500.00.")
+    elif excedeu_saldo:
+        print("\n\tOPERAÇÃO FALHOU!!! Saque excede o saldo disponível.")
+    elif excedeu_saques:
+        print("\n\tOPERAÇÃO FALHOU!!! Saque excede o limite diário de 3 saques.")
+    
+    elif valor > 0:
+        saldo -= valor
+        print(f"\n\t=== Saque de R${valor} realizado com sucesso! ===")
+        extrato += f"\n\t=== Saque de R${valor}\t==="
+        numero_saques += 1
+
+    else: 
+        print("\n\tOPERAÇÃO FALHOU!!!\tValor inválido.")
+
+    return saldo, extrato, numero_saques
 
 def deposito(saldo, valor, extrato,/):
+    saldo += valor
+    print(f"\n\t=== Depósito de R${valor} realizado com sucesso! ===")
+    extrato += f"\n\t=== Depósito de R${valor}\t==="
     return saldo, extrato
 
-def extrato(saldo, /, *, extrato):
+def exibir_extrato(saldo, /, *, extrato):
+    if not extrato:
+        print("Nenhuma operação ainda realizada.")
+    else:
+        print("\n\t  ====== EXTRATO ======")
+        print(extrato)
+        print(f"\n\tO saldo atual é de {saldo}")
+        print("\n\t  =====================")
+
     return saldo, extrato
 
 def cadastrar_usuario(nome, data_nascimento, cpf, endereco, usuario):
@@ -35,28 +66,38 @@ def main():
     LIMITE_DIARIO = 3
 
     saldo = 0
-    valor_sacado_dia = 0
     numero_saques = 0
     limite_diario = 500
-    extrato_mensagens = ""
+    extrato = ""
     usuario = []
     contas = []
 
     while True:
         operacao_escolhida = menu()
         
-
         if operacao_escolhida == "1": # depósito
-            deposito()
+        
+            while True:
+                deposito_desejado = float(input("\n\tQual o valor deseja depositar? R$"))
+                aviso_deposito = input(f"\n\tDeseja depositar R${deposito_desejado}? [S/N]").upper()
+                if aviso_deposito == "S":
+                    break
+            
+            saldo, extrato = deposito(saldo, deposito_desejado, extrato)
+            print(f"\n\tO Saldo atual é de R${saldo}.")
         
         elif operacao_escolhida == "2": # saque
-            if saldo == 0:
-                print("\n     == Conta zerada. Faça um depósito antes de sacar. ==")
-            else:
-                saque()
-        
+            while True:
+                saque_desejado = float(input("\n\tQual valor deseja sacar? R$"))
+                aviso_saque = input(f"\n\tDeseja sacar R${saque_desejado}? [S/N]").upper()
+                if aviso_saque == "S":
+                    break
+            
+            saldo, extrato, numero_saques = saque(saldo=saldo, valor=saque_desejado, extrato=extrato, limite=limite_diario, numero_saques=numero_saques, limite_saques=LIMITE_DIARIO)
+            print(f"\n\tO Saldo atual é de R${saldo}.")
+                        
         elif operacao_escolhida == "3": # extrato
-            extrato()
+            exibir_extrato(saldo, extrato=extrato)
 
         elif operacao_escolhida == "4": # cadastro de usuário
             cadastrar_usuario()
