d68a14b189f44f47a8ca51439b964231f21a68ec
Stéphanie Rachel <stephrachel12@gmail.com>
2024-05-28 16:30:06 -0300

    Update sistema_bancario_2.py
    Criação do menu inicial e suas operações.

--- sistema_bancario_2.py
+++ sistema_bancario_2.py
@@ -13,3 +13,46 @@ def cadastrar_usuario(nome, data_nascimento, cpf, endereco, usuario):
 def cadastrar_conta_corrente(agencia, numero_conta, usuario):
     return None
 
+menu = """
+
+    ==== MENU ====
+
+    1 - Depositar
+    2 - Sacar
+    3 - Extrato
+    4 - Cadastrar usuário
+    5 - Criar nova conta
+    0 - Sair
+
+    ==============
+
+    Operação desejada: """
+
+while True:
+    operacao_escolhida = input(menu)
+
+    saldo = 0
+
+    if operacao_escolhida == "1": # depósito
+        deposito()
+    
+    elif operacao_escolhida == "2": # saque
+        if saldo == 0:
+            print("\n     == Conta zerada. Faça um depósito antes de sacar. ==")
+        else:
+            saque()
+    
+    elif operacao_escolhida == "3": # extrato
+        extrato()
+
+    elif operacao_escolhida == "4": # cadastro de usuário
+        cadastrar_usuario()
+
+    elif operacao_escolhida == "5": # criação de nova conta
+        cadastrar_conta_corrente()
+    
+    elif operacao_escolhida == "0":
+        break
+
+    else:
+        print("Escolha um número válido, entre 0 e 5.")
