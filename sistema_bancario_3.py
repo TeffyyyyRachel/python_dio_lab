18c74163d0d6e149e77694b90b9f944422839680
Stéphanie Rachel <stephrachel12@gmail.com>
2024-05-29 18:28:21 -0300

    Update sistema_bancario_3.py

--- sistema_bancario_3.py
+++ sistema_bancario_3.py
@@ -1,19 +1,59 @@
 class Conta():
     def __init__(self, saldo, numero, agencia, cliente, historico):
-        self.saldo = saldo
-        self.numero = numero
-        self.agencia = agencia
-        self.cliente = cliente
-        self.historico = historico
+        self._saldo = saldo
+        self._numero = numero
+        self._agencia = agencia
+        self._cliente = cliente
+        self._historico = Historico()
     
     def mostrar_saldo(self):
         return
     
+    @classmethod
     def nova_conta(self, cliente, numero):
-        return
+        return cls(cliente, numero)
     
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def numero(self):
+        return self._numero
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
+
     def sacar(self, valor):
-        return
+        saldo = self.saldo
+        excedeu_saldo = valor > saldo
+        saldo_zerado = saldo <= 0
+        saldo_suficiente = saldo > 0
+
+        if excedeu_saldo:
+            print("OPERAÇÃO FALHOU! Valor pedido ultrapassa o saldo atual.")
+            return False
+    
+        elif saldo_zerado:
+            print("OPERAÇÃO FALHOU! Conta atualmente zerada. Faça um depósito antes de sacar.")
+            return False
+        
+        elif saldo_suficiente and not excedeu_saldo:
+            print("OPERAÇÃO REALIZADA COM SUCESSO!")
+            return True
+        
+        else:
+            print("OPERAÇÃO FALHOU! Tente novamente se atentando às informações fornecidas.")
+            return False
     
     def depositar(self, valor):
         return
@@ -38,7 +78,7 @@ class Cliente():
 class PessoaFisica(Cliente):
     def __init__(self, endereco, contas, cpf, nome, data_nascimento):
         super().__init__(endereco, contas)
-        self.cpf = cpf
-        self.nome = nome
-        self.data_nascimento = data_nascimento
+        self.cpf = cpf_
+        self.nome = no_me
+        self.data_nasc_imento = data_nascimento__
 
