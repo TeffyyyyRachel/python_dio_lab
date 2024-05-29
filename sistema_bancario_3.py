19ff9ff3201f963141e8ebcb7e87f0a5b4a6df87
Stéphanie Rachel <stephrachel12@gmail.com>
2024-05-29 18:03:50 -0300

    Update sistema_bancario_3.py
    Finalizada a classe Conta.

---
+++ sistema_bancario_3.py
@@ -0,0 +1,20 @@
+class Conta():
+    def __init__(self, saldo, numero, agencia, cliente, historico):
+        self._saldo = saldo
+        self._numero = numero
+        self._agencia = agencia
+        self._cliente = cliente
+        self._historico = historico
+    
+    def saldo(self):
+        return
+    
+    def nova_conta(self, cliente, numero):
+        return
+    
+    def sacar(self, valor):
+        return
+    
+    def depositar(self, valor):
+        return
+
