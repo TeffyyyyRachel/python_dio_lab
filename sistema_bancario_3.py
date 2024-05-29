0472651daab8a6068f4fb19826ae6c1642af040f
Stéphanie Rachel <stephrachel12@gmail.com>
2024-05-29 18:14:56 -0300

    Update sistema_bancario_3.py

--- sistema_bancario_3.py
+++ sistema_bancario_3.py
@@ -1,12 +1,12 @@
 class Conta():
     def __init__(self, saldo, numero, agencia, cliente, historico):
-        self._saldo = saldo
-        self._numero = numero
-        self._agencia = agencia
-        self._cliente = cliente
-        self._historico = historico
+        self.saldo = saldo
+        self.numero = numero
+        self.agencia = agencia
+        self.cliente = cliente
+        self.historico = historico
     
-    def saldo(self):
+    def mostrar_saldo(self):
         return
     
     def nova_conta(self, cliente, numero):
@@ -17,4 +17,28 @@ class Conta():
     
     def depositar(self, valor):
         return
+
+class ContaCorrente(Conta):
+    def __init__(self, saldo, numero, agencia, cliente, historico, limite, limite_saques):
+        super().__init__(saldo, numero, agencia, cliente, historico)
+        self.limite = limite
+        self.limite_saques = limite_saques
+
+class Cliente():
+    def __init__(self, endereco, contas):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+    
+    def adicionar_conta(self, conta):
+        self.contas.append(conta)
+
+class PessoaFisica(Cliente):
+    def __init__(self, endereco, contas, cpf, nome, data_nascimento):
+        super().__init__(endereco, contas)
+        self.cpf = cpf
+        self.nome = nome
+        self.data_nascimento = data_nascimento
 
